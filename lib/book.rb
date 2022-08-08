5bf26b802b9a0a5a457d057dcaa407f574094cb5
Lucas Duncan <lucasdonuts@gmail.com>
2022-08-08 16:13:24 -0500

    All tests passing

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+require 'pry'
 
+class Book
+  attr_accessor :title, :author, :page_count, :genre
 
+  def initialize( title )
+    @title = title
+    @author
+    @page_count
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+end
+
+# binding.pry
